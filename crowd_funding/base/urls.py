6aae35b964f1f9cb984e5edb7b76ad1ec9295e04
Odiase <odiaseefosa18@gmail.com>
2022-12-17 20:56:43 +0100

    Added all Other templates

--- crowd_funding/base/urls.py
+++ crowd_funding/base/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, sign_in
 
 urlpatterns = [
     path("", home, name="home"),
+    path("sign_in", sign_in, name="sign_in"),
 ]
