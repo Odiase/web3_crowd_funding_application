328fe5cb209f286f4de4e19f25d294fa5b9b705b
Odiase <odiaseefosa18@gmail.com>
2022-12-30 14:51:44 +0100

    Removed more fields from my crowdfund table

--- crowd_funding/crowd_fund/models.py
+++ crowd_funding/crowd_fund/models.py
@@ -7,12 +7,10 @@ class CrowdFund(models.Model):
     '''Crowd Funding Table Schema'''
 
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="crowd_funds")
-    wallet_address = models.CharField(max_length=100)
     name = models.CharField(max_length=100, unique=True)
     created = models.DateTimeField(auto_now_add=True)
     image = models.ImageField()
-    description = models.TextField()
 
     def __str__(self):
-        return self.name
+        return self.name + " CrowdFund."
     
