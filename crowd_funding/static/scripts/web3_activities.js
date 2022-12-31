529e480f473b3f8977aeec0d3e964f2af4391715
Odiase <odiaseefosa18@gmail.com>
2022-12-31 08:39:58 +0100

    created a new function that initiates a ethereum web3 object

--- crowd_funding/static/scripts/web3_activities.js
+++ crowd_funding/static/scripts/web3_activities.js
@@ -1,30 +1,36 @@
-// this file checks if a web3 extension(Mostly MetaMask) exists as an extension in the user's browser
-async function check_and_connect() {
-    /** Checking if a metamask extension is available and trying to access it.....of course with the user's permission */
-    if (window.ethereum) {
+function get_web3_object() {
+    if(window.ethereum) {
+        // assinging web3 object to global window web3 3 variable
         window.web3 = new Web3(ethereum);
-        var web3_obj = window.web3;
-        console.log(web3_obj)
-        try {
-            // trying to access the user web3 account on meta mask or any other web3 provider, if any
-             var accounts = await window.web3.eth.requestAccounts();
-
-             // redirects the user to the home page.
-             if (accounts) {
-                window.location.replace('http://127.0.0.1:8000');
-             }
-             console.log(accounts)
-        } catch (error) {
-            if (error["message"] == "Already processing eth_requestAccounts. Please wait.") {
-                window.alert("You seem to have denied access to a previous attempt to access your wallet, please clarify that.")
-            }
-            console.error("User denied account access");
-        }
-    }
-    else {
+        web3_obj = window.web3;
+    }else {
         window.alert("Note: You do not have MetaMask or any Web3 Extensions activated");
     }
 }
+
+
+
+
+async function check_and_connect() {
+    /** Checking if a metamask extension is available and trying to access it.....of course with the user's permission */
+    web3_obj = get_web3_object();
+    
+    try {
+        // trying to access the user web3 account on meta mask or any other web3 provider, if any
+         var accounts = await web3_obj.eth.requestAccounts();
+         // redirects the user to the home page.
+         if (accounts) {
+            window.location.replace('http://127.0.0.1:8000');
+         }
+         console.log(accounts)
+    } catch (error) {
+        if (error["message"] == "Already processing eth_requestAccounts. Please wait.") {
+            window.alert("You seem to have denied access to a previous attempt to access your wallet, please clarify that.")
+        }
+        console.error("User denied account access");
+    }
+}
+
 
 
 
