cd2b86a1697c7970c180d0370644794d6568be4c
Odiase <odiaseefosa18@gmail.com>
2022-12-31 14:10:41 +0100

    made changes to abi, seeing thatchanges were applied to the smart contract

--- crowd_funding/static/scripts/web3_activities.js
+++ crowd_funding/static/scripts/web3_activities.js
@@ -21,6 +21,7 @@ async function get_wallet() {
          var accounts = await web3_obj.eth.requestAccounts();
          // redirects the user to the home page.
          if (accounts) {
+            localStorage.setItem('account', accounts[0])
             window.location.replace('http://127.0.0.1:8000/crowd_fund/create');
          }
          console.log(accounts)
@@ -34,13 +35,38 @@ async function get_wallet() {
 
 function create_crowd_fund() {
     web3_obj = get_web3_object();
-    const ABI = require('./crowd_fund_factory_abi.json');
-    contractAddress = "0x69fffF4Ef84CEB8cCE746daeC3A47949504d0f1F";
-    const contract = web3_obj.eth.Contract(ABI, contractAddress);
-    console.log(contract)
+    let contract;
+
+    // Retrieve the ABI from the crowd_fund_factory_abi.json file
+    fetch('http://127.0.0.1:8000/static/scripts/crowd_fund_factory_abi.json')
+    .then(response => response.json())
+    .then(ABI => {
+        contractAddress = "0xea1d2f4035302812495CA6dcf39C309E21422618"
+        // Use the ABI to define the interface of the smart contract
+        contract = new web3_obj.eth.Contract(ABI.abi, contractAddress);
+        console.log(contract);
+
+
+        // create crowd fund
+        accountAddress = get_wallet_address()
+        contract.methods.createCrowdFundContract("UkrRusJs", "Desc", "Efosa").send({from: accountAddress})
+        .then(functionResult => {
+            // Use the function result
+        2})
+        .catch(error => {console.log(error)});
+    });
+
+
 }
 
-
+function get_wallet_address(){
+    if (localStorage.getItem('account')) {
+        return localStorage.getItem('account');
+    }
+    else{
+        window.location.replace("http://127.0.0.1:8000/connect_wallet");
+    }
+}
 
 // function is_connected() {
 //     web3_obj = get_web3_object();
