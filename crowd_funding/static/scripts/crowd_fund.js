fd3061e737c07041258e334941e1cd2cb8fa4276
Odiase <odiaseefosa18@gmail.com>
2023-01-02 10:32:54 +0100

    Made the get_wallet redirect section more dynamc

--- crowd_funding/static/scripts/crowd_fund.js
+++ crowd_funding/static/scripts/crowd_fund.js
@@ -1,35 +1,41 @@
+import { get_web3_object, get_wallet } from './web3_activities.js';
+
 let crowd_fund_form = document.getElementById("crowd_fund_form");
-console.log(crowd_fund_form['owner_name'].value)
 
 
 
 
 function create_crowd_fund() {
-    web3_obj = get_web3_object();
+    let web3_obj = get_web3_object();
     let contract;
 
     // Retrieve the ABI from the crowd_fund_factory_abi.json file
     fetch('http://127.0.0.1:8000/static/scripts/crowd_fund_factory_abi.json')
     .then(response => response.json())
     .then(ABI => {
-        contractAddress = "0xea1d2f4035302812495CA6dcf39C309E21422618"
+        let contractAddress = "0xea1d2f4035302812495CA6dcf39C309E21422618"
         // Use the ABI to define the interface of the smart contract
         contract = new web3_obj.eth.Contract(ABI.abi, contractAddress);
-        console.log(contract);
-
 
         // create crowd fund
         let accountAddress = get_wallet_address();
-        let create_fund_form = get_create_form_data();
+        let create_fund_form = get_form_data();
 
         contract.methods.createCrowdFundContract(create_fund_form[0], create_fund_form[2], create_fund_form[1]).send({from: accountAddress})
         .then(functionResult => {
             // Use the function result
         2})
         .catch(error => {
-            if (error.message.includes("User denied transaction signature")) {
+            // this runs if the user has rejected the transaction
+            if (error.message.includes("User denied transaction signature") || error.code == 4001) {
                 window.alert('You Rejected The Transaction')
-            }else{
+            }
+            // this runs if no wallet has been linked to the application
+            if (error.code == 4100) {
+                get_wallet('crowd_fund/create');
+                
+            }
+            else{
                 window.alert("A Crowd Fund With That Name Already Exists.")
             }
         });
@@ -47,7 +53,7 @@ function get_wallet_address(){
     }
 }
 
-function get_create_form_data() {
+function get_form_data() {
     let name = crowd_fund_form['fund_name'].value;
     let owner_name = crowd_fund_form['owner_name'].value;
     let description = crowd_fund_form['description'].value;
@@ -55,7 +61,7 @@ function get_create_form_data() {
     return [name, owner_name, description];
 }
 
-console.log(get_create_form_data())
+console.log(get_form_data())
 
 
 // event listener
--- crowd_funding/static/scripts/web3_activities.js
+++ crowd_funding/static/scripts/web3_activities.js
@@ -1,8 +1,8 @@
-function get_web3_object() {
+export function get_web3_object() {
     if(window.ethereum) {
         // assinging web3 object to global window web3 3 variable
         window.web3 = new Web3(ethereum);
-        web3_obj = window.web3;
+        let web3_obj = window.web3;
         return web3_obj;
     }else {
         window.alert("Note: You do not have MetaMask or any Web3 Extensions activated");
@@ -12,19 +12,26 @@ function get_web3_object() {
 
 
 
-async function get_wallet() {
+export async function get_wallet(redirect_url) {
     /** Checking if a metamask extension is available and trying to access it.....of course with the user's permission */
-    web3_obj = get_web3_object();
+    let web3_obj = get_web3_object();
     
     try {
         // trying to access the user web3 account on meta mask or any other web3 provider, if any
          var accounts = await web3_obj.eth.requestAccounts();
          // redirects the user to the home page.
          if (accounts) {
-            localStorage.setItem('account', accounts[0])
-            window.location.replace('http://127.0.0.1:8000/crowd_fund/create');
+            // saving the account in local storage
+            localStorage.setItem('account', accounts[0]);
+
+            // redirection
+            let current_url = window.location.href;
+            let url_to_redirect = `http://127.0.0.1:8000/${redirect_url}`;
+            if (current_url == url_to_redirect) {}
+            else{  
+                window.location.replace(url_to_redirect);
+            }
          }
-         console.log(accounts)
     } catch (error) {
         if (error["message"] == "Already processing eth_requestAccounts. Please wait.") {
             window.alert("You seem to have denied access to a previous attempt to access your wallet, please clarify that.")
