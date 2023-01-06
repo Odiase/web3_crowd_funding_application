503fe070fb4d910f480d12629517a069aada1543
Odiase <odiaseefosa18@gmail.com>
2023-01-06 14:46:35 +0100

    new change

--- crowd_funding/crowd_fund/views.py
+++ crowd_funding/crowd_fund/views.py
@@ -1,5 +1,5 @@
 # third party packages imports
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 
 #local imports
@@ -15,13 +15,21 @@ def create_crowd_fund(request):
         image = request.FILES['image']
 
         crowd_fund = CrowdFund.objects.create(owner=owner, name=crowd_fund_name, image=image)
+        return redirect('single_crowd_fund', crowd_fund_name)
 
     return render(request, "create_crowd_funding.html")
 
-
 @login_required(login_url="sign_in")
 def single_crowd_fund(request, name):
+    # crowd_fund_image = get_object_or_404(CrowdFund, name=name)
+    crowd_fund_image = CrowdFund.objects.get(name=name).image
+    image = ""
+
+    if crowd_fund_image:
+        image = crowd_fund_image
+
     context = {
-        "name" : name
+        "name" : name, 
+        "image" : image
     }
     return render(request, "single_crowd_fund.html", context)
