7fd68d6e795906a913e94a37196ce35da5e310a3
Odiase <odiaseefosa18@gmail.com>
2023-01-08 11:07:21 +0100

    Created Fund Function that'll handle the sending of fund to the crowd_fun's address

--- crowd_funding/static/scripts/web3_single_crowd_fund.js
+++ crowd_funding/static/scripts/web3_single_crowd_fund.js
@@ -8,17 +8,18 @@ let single_crowd_fund_container = document.querySelector(".single_crowd_section"
 let amount_raised_element = document.getElementById("amount_raised");
 let num_of_funders_element = document.getElementById("num_of_funders");
 let description_text = document.getElementById("description_text");
+let send_fund_form = document.getElementById("send_fund_form");
 
 
 /** Functions */
 
 // @params: name - this is the name of the crowd_fund
-// @notice - the web3_obj gets the web3_object from the web3_activities js, so as to interact with the browser as a web3 browser
+// @notice - the web3_obj variable gets its value from the web3_activities js, so as to interact with the browser as a web3 browser
 async function get_single_crowd_fund(name) {
     let crowd_fund_info;
 
     // getting web3 instance
-    let web3_obj = get_web3_object();
+    // let web3_obj = get_web3_object();
 
     //getting smart contract
     let contract = await get_smart_contract()
@@ -74,12 +75,34 @@ async function fund_a_crowd_fund(funder_name, crowd_fund_name, amount){
     let web3_obj = get_web3_object();
 
     // get smart contract
-    let contract = get_smart_contract();
+    let contract = await get_smart_contract();
 
     // create transaction
+    try{
+        const sender_account_address = get_wallet_address()
+        const transaction = await contract.methods.fund(funder_name, crowd_fund_name).send({
+            "from" : sender_account_address,
+            "value" : web3_obj.utils.toWei(`${amount}`, 'ether'),
+            "gas" : "100000"
+        })
+        window.alert("Funds Sent!")
+    }
+    catch(error) {
+        if (error.code == 4001) {
+            window.alert("You Rejected This Transaction.")
+        }
+        console.log(error)
+    }
 
 }
 
+function get_fund_data() {
+
+}
 
 //event listener
 window.addEventListener("load", crowd_fund_exists(crowd_fund_name));
+send_fund_form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    fund_a_crowd_fund(send_fund_form['sender_name'].value, crowd_fund_name, send_fund_form['amount'].value);
+})
